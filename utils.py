373a73acff6f1a7a827ec17ff2f827d31a68d242
Maleesha Gimhan ☢️ <78734430+SL-MGx03@users.noreply.github.com>
2026-03-26 23:46:36 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -29,7 +29,7 @@ vector_store = MongoDBAtlasVectorSearch(
     index_name="vector_index" 
 )
 
-def get_retriver():
+def get_retriever():
     return (vector_store.as_retriever(search_kwargs={"k": 5}))
 
 def get_sessions_collection():
