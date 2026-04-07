889a288f59e3b528539e871ed5db52696c90dbb5
Maleesha Gimhan ☢️ <78734430+SL-MGx03@users.noreply.github.com>
2026-04-07 18:05:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,11 @@ from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from typing import Optional
 
+from langchain.messages import HumanMessage
+
 from timetable_generater import get_timetable
 from assistant_model import rag_chain
+from se_agent_model import se_agent
 
 origins = [
     "https://slmgx.live",
@@ -23,6 +26,12 @@ app.add_middleware(
 
 class ChatQuery(BaseModel):
     message: str
+
+
+class SEAgentQuery(BaseModel):
+    message: str
+    thread_id: str = "default_session" # ID for the 24h MongoDB memory
+
 
 @app.post("/chat")
 async def chat(query: ChatQuery):
@@ -56,6 +65,26 @@ async def timetable_chat(
         return get_timetable(user_id, message, file_bytes, content_type)
     except Exception as e:
         print(f"Error in timetable-chat: {e}")
+        return {"error": str(e)}
+
+
+@app.post("/se_agent")
+async def se_agent_chat(query: SEAgentQuery):
+
+    agent = se_agent()
+    try:
+        config = {"configurable": {"thread_id": query.thread_id}}
+        input_state = {"messages": [HumanMessage(content=query.message)]}
+        result = agent.invoke(input_state, config)
+        final_answer = result["messages"][-1].content
+        
+        return {
+            "answer": final_answer,
+            "thread_id": query.thread_id
+        }
+        
+    except Exception as e:
+        print(f"SE AGENT ERROR: {str(e)}")
         return {"error": str(e)}
 
 
